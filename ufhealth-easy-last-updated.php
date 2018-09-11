cd70accf34e0dbc5740e147f141921e6736d7b8f
Chris Wiegman <cwiegman@ufl.edu>
2018-09-11 10:55:16 -0400

    version bump

--- ufhealth-easy-last-updated.php
+++ ufhealth-easy-last-updated.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: UF Health Easy Last Updated
  * Description: Shows the last updated time for content on the content type tables for easier auditing.
- * Version: 1.0
+ * Version: 1.1
  * Text Domain: ufhealth-easy-last-updated
  * Domain Path: /languages
  * Author: UF Health
@@ -12,7 +12,7 @@
  * @package UFHealth\Easy_Last_Updated
  */
 
-define( 'UFHEALTH_EASY_LAST_UPDATED_VERSION', '1.0' );
+define( 'UFHEALTH_EASY_LAST_UPDATED_VERSION', '1.1' );
 define( 'UFHEALTH_EASY_LAST_UPDATED_URL', plugin_dir_url( __FILE__ ) );
 
 add_action( 'plugins_loaded', 'ufhealth_easy_last_updated_loader' );
